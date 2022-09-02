67eae35b95dfbdf5de6e5f0af4883acff10c6cd3
vic233333 <ddbb520db@163.com>
2022-09-02 10:30:48 +0800

    添加注释

--- 2022.09.02/HelloWord.java
+++ 2022.09.02/HelloWord.java
@@ -1,14 +1,10 @@
-
-/**
- * 在这里给出对类 HelloWord 的描述。
- * 
- * @作者（你的名字）
- * @版本（一个版本号或者一个日期）
+/*
+ * 使用BlueJ打开
  */
 public class HelloWord
 {
     public static void main(){
-        System.out.print("Hellow World");//打完之后光标还在同一行
         System.out.println("Hello World");//打完之后光标自动换行
+        System.out.print("Hellow World");//打完之后光标还在同一行
     }
 }
