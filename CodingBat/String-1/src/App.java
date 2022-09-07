29f47dda48c8eed618639921cc6cb7044ddd0cf3
vic233333 <ddbb520db@163.com>
2022-09-07 08:59:01 +0800

    codeBat String-1 0-3

---
+++ CodingBat/String-1/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    public String makeOutWord(String out, String word) {
+        String first = out.substring(0, 2);
+        String last = out.substring(2);
+        return first + word + last;
+    }
+}
