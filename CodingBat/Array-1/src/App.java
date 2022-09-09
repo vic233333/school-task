21573e939f455f35a4f9b6572f87f41a70aa6297
vic233333 <ddbb520db@163.com>
2022-09-09 11:15:58 +0800

    sum3 rotateLeft3

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -39,4 +39,21 @@ public class App {
             return false;
         }
     }
+
+    public int sum3(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    public int[] rotateLeft3(int[] nums) {
+        int temp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i - 1] = nums[i];
+        }
+        nums[nums.length - 1] = temp;
+        return nums;
+    }
 }
