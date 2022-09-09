2e98cf580b67e8e33c496ab7068afe884435f18e
vic233333 <ddbb520db@163.com>
2022-09-09 10:46:47 +0800

    sameFirstLast makePi

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -4,11 +4,33 @@ public class App {
     }
 
     public boolean firstLast6(int[] nums) {
-        if (nums[0] == 6
+        if (nums.length == 0) {
+            return false;
+        } else if (nums[0] == 6
                 || nums[nums.length - 1] == 6) {
             return true;
         } else {
             return false;
         }
     }
+
+    public boolean sameFirstLast(int[] nums) {
+        if (nums.length == 0) {
+            return false;
+        } else if (nums[0] == nums[nums.length - 1]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int[] makePi() {
+        int[] Pi = new int[3];
+        Pi[0] = 3;
+        Pi[1] = 1;
+        Pi[2] = 4;
+        return Pi;
+    }
+
+    
 }
