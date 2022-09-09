aba39b21d424c558eb8582b369e3ef8abfaa911d
vic233333 <ddbb520db@163.com>
2022-09-09 11:11:39 +0800

    commonEnd

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -32,5 +32,11 @@ public class App {
         return Pi;
     }
 
-    
+    public boolean commonEnd(int[] a, int[] b) {
+        if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
