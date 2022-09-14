9716746f1b2f82391d2a123a1dac6f1c2a9c7bc9
vic233333 <ddbb520db@163.com>
2022-09-14 09:37:44 +0800

    no23

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -104,4 +104,13 @@ public class App {
         }
         return false;
     }
+
+    public boolean no23(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2 || nums[i] == 3) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
