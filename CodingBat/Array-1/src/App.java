977c90924dec9af411ba4b8d1ed9444e8abd21a0
vic233333 <ddbb520db@163.com>
2022-09-14 09:42:26 +0800

    makeLast

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -113,4 +113,10 @@ public class App {
         }
         return true;
     }
+
+    public int[] makeLast(int[] nums) {
+        int ans[] = new int[nums.length * 2];
+        ans[ans.length - 1] = nums[nums.length - 1];
+        return ans;
+    }
 }
