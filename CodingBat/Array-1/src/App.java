39ef3a2c0e76418255097e9dfd6e47350388c3a5
vic233333 <ddbb520db@163.com>
2022-09-16 10:27:55 +0800

    makeMiddle

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -165,4 +165,11 @@ public class App {
         }
         return sum_a >= sum_b ? a : b;
     }
+
+    public int[] makeMiddle(int[] nums) {
+        int[] ans = new int[2];
+        ans[0] = nums[nums.length / 2 - 1];
+        ans[1] = nums[nums.length / 2];
+        return ans;
+    }
 }
