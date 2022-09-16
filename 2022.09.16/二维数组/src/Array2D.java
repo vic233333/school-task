992d1ca96e02ef0b3c5441237444ea6760a79b3e
vic233333 <ddbb520db@163.com>
2022-09-16 10:43:07 +0800

    定义二维数组并从1到n赋值，最后输出

--- 2022.09.16/二维数组/src/Array2D.java
+++ 2022.09.16/二维数组/src/Array2D.java
@@ -13,9 +13,22 @@ public class Array2D {
         }
     }
 
-    public static void main(String[] args) {
-        int[][] arr = make2D(3, 4);
-        print2D(arr);
+    public static int[][] make1ToN2Darray(int row, int col) {
+        int[][] arr = new int[row][col];
+        int n = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = n;
+                n++;
+            }
+        }
+        return arr;
+    }
 
+    public static void main(String[] args) {
+        int[][] arr_1 = make2D(3, 4);
+        print2D(arr_1);
+        int[][] arr_2 = make1ToN2Darray(3, 4);
+        print2D(arr_2);
     }
 }
