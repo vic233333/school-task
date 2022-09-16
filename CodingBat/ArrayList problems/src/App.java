9920eb93b7f12db1e9f38ddced2de3036e68f632
vic233333 <ddbb520db@163.com>
2022-09-16 11:08:31 +0800

    listSumEvenIndexes

---
+++ CodingBat/ArrayList problems/src/App.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;//引入ArrayList库
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int listSumEvenIndexes(List<Integer> numsList) {
+        int sum = 0;
+        for (int i = 0; i < numsList.size(); i+=2) {
+            sum += numsList.get(i);
+        }
+        return sum;
+    }
+}
