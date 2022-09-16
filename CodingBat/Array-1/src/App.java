9c39d01835da30fff46db87b87898a4575ff4ce3
vic233333 <ddbb520db@163.com>
2022-09-16 10:17:40 +0800

    start1

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -142,4 +142,15 @@ public class App {
         }
         return nums;
     }
+
+    public int start1(int[] a, int[] b) {
+        int sum = 0;
+        if (a.length > 0 && a[0] == 1) {
+            sum++;
+        }
+        if (b.length > 0 && b[0] == 1) {
+            sum++;
+        }
+        return sum;
+    }
 }
