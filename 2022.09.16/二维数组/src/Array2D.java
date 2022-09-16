b8b07d197c71aab0e0b5659192bf39c1e48d2799
vic233333 <ddbb520db@163.com>
2022-09-16 10:43:26 +0800

    打错字了

--- 2022.09.16/二维数组/src/Array2D.java
+++ 2022.09.16/二维数组/src/Array2D.java
@@ -13,7 +13,7 @@ public class Array2D {
         }
     }
 
-    public static int[][] make1ToN2Darray(int row, int col) {
+    public static int[][] make1ToN2DArray(int row, int col) {
         int[][] arr = new int[row][col];
         int n = 1;
         for (int i = 0; i < arr.length; i++) {
