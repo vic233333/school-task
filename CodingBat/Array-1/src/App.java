c19299ace25490bb065bf63a711b79dd34e87ae9
vic233333 <ddbb520db@163.com>
2022-09-21 14:24:24 +0800

    midThree

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -190,4 +190,12 @@ public class App {
         nums[nums.length - 1] = temp;
         return nums;
     }
+
+    public int[] midThree(int[] nums) {
+        int[] ans = new int[3];
+        ans[0] = nums[nums.length / 2 - 1];
+        ans[1] = nums[nums.length / 2];
+        ans[2] = nums[nums.length / 2 + 1];
+        return ans;
+    }
 }
