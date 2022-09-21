020b5cfa41ea4e49ae57247036419651b9cefe6f
vic233333 <ddbb520db@163.com>
2022-09-21 14:22:39 +0800

    swapEnds

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -183,4 +183,11 @@ public class App {
         }
         return ans;
     }
+
+    public int[] swapEnds(int[] nums) {
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        return nums;
+    }
 }
