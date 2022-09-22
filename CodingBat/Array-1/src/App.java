3248b1253a0625e9366397f88a6fa2a7d4763c45
vic233333 <ddbb520db@163.com>
2022-09-22 09:09:07 +0800

    unlucky1

--- CodingBat/Array-1/src/App.java
+++ CodingBat/Array-1/src/App.java
@@ -217,4 +217,25 @@ public class App {
             return ans;
         }
     }
+
+    public boolean unlucky1(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        } else if (nums.length == 2) {
+            if (nums[0] == 1 && nums[1] == 3) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (nums[0] == 1 && nums[1] == 3 ||
+                    nums[1] == 1 && nums[2] == 3 ||
+                    nums[nums.length - 1] == 3
+                            && nums[nums.length - 2] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
