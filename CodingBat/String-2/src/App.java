99ce7c30a640e7d50b96f4b2735c20faac23cd36
vic233333 <ddbb520db@163.com>
2022-09-26 14:40:00 +0800

    endOther

--- CodingBat/String-2/src/App.java
+++ CodingBat/String-2/src/App.java
@@ -73,6 +73,26 @@ public class App {
     }
 
     public boolean endOther(String a, String b) {
-        
-    }    
+        a = a.toLowerCase();
+        int aLen = a.length();
+
+        b = b.toLowerCase();
+        int bLen = b.length();
+
+        if (aLen < bLen) {
+            String temp = b.substring(bLen - aLen, bLen);
+            if (temp.compareTo(a) == 0)
+                return true;
+            else
+                return false;
+
+        } else {
+            String temp = a.substring(aLen - bLen, aLen);
+            if (temp.compareTo(b) == 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
 }
