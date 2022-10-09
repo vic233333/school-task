8e02d627bd929116698fc156d7c6713ee074f6fe
vic233333 <ddbb520db@163.com>
2022-10-09 21:07:15 +0800

    xyzThere method 3 格式修正

--- CodingBat/String-2/src/App.java
+++ CodingBat/String-2/src/App.java
@@ -214,8 +214,8 @@ public class App {
     public boolean xyzThere(String str) {
         str = " " + str;
         for (int i = 0; i < str.length() - 3; i++) {
-            if (!str.substring(i, i+1).equals(".") &&
-            str.substring(i+1, i+4).equals("xyz")) {
+            if (!str.substring(i, i + 1).equals(".") &&
+                    str.substring(i + 1, i + 4).equals("xyz")) {
                 return true;
             }
         }
