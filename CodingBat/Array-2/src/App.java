21003979768f47f961971d889e270fcb635ca4f5
vic233333 <ddbb520db@163.com>
2022-10-17 14:54:39 +0800

    bigDiff

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -12,4 +12,30 @@ public class App {
         }
         return sum;
     }
+
+    // bigDiff method 1
+    public int bigDiff (int[] nums) {
+        int maxn = nums[0];
+        int minn = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxn) {
+                maxn = nums[i];
+            }
+            if (nums[i] < minn) {
+                minn = nums[i];
+            }
+        }
+        return maxn - minn;
+    }
+
+    // bigDiff method 2
+    public int bigDiff(int[] nums) {
+        int maxn = nums[0];
+        int minn = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            maxn = Math.max(nums[i], maxn);
+            minn = Math.min(nums[i], minn);
+        }
+        return maxn - minn;
+    }
 }
