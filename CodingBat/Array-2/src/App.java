c585f5ff12d0130cf41288bcef839e7c029a9862
vic233333 <ddbb520db@163.com>
2022-10-24 14:53:24 +0800

    twoTwo

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -357,4 +357,23 @@ public class App {
         }
         return count == 3 && found == false;
     }
+
+    public boolean twoTwo(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i == 0 && nums[i] == 2 && nums[i + 1] != 2) {
+                return false;
+            }
+            if (i == nums.length - 2 && nums[i] != 2 && nums[i + 1] == 2F) {
+                return false;
+            }
+            if (i < nums.length - 2 && nums[i] != 2 &&
+                    nums[i + 1] == 2 && nums[i + 2] != 2) {
+                return false;
+            }
+        }
+        if (nums.length == 1 && nums[0] == 2) {
+            return false;
+        }
+        return true;
+    }
 }
