2efd1618c9386ba2845599ae5020acda5078854e
vic233333 <ddbb520db@163.com>
2022-10-24 13:31:22 +0800

    more14

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -158,4 +158,18 @@ public class App {
         }
         return sum == 8;
     }
+
+    public boolean more14(int[] nums) {
+        int count1 = 0;
+        int count4 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count1 ++;
+            }
+            if (nums[i] == 4) {
+                count4 ++;
+            }
+        }
+        return count1 > count4;
+    }
 }
