2f75bc10f0c835bb29ed2f07255436980fb2e2b7
vic233333 <ddbb520db@163.com>
2022-10-24 14:37:52 +0800

    haveThree method 2

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -326,19 +326,35 @@ public class App {
     public boolean haveThree(int[] nums) {
         int count = 0;
         boolean flag = false;
-        for(int i=0;i<nums.length;i++){
-          if(i<nums.length-1&&nums[i]==3&&nums[i+1]==3){
-            flag = true;
-          }
-          if(nums[i]==3){
-            count++;
-          }
+        for (int i = 0; i < nums.length; i++) {
+            if (i < nums.length - 1 && nums[i] == 3 && nums[i + 1] == 3) {
+                flag = true;
+            }
+            if (nums[i] == 3) {
+                count++;
+            }
         }
-        if(count==3&&flag==false){
-          return true;
+        // 此处不够简洁
+        // 可以参考method2
+        if (count == 3 && flag == false) {
+            return true;
+        } else {
+            return false;
         }
-        else{
-          return false;
+    }
+
+    // haveThree method 2
+    public boolean haveThree(int[] nums) {
+        int count = 0;
+        boolean found = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < nums.length - 1 && nums[i] == 3 && nums[i + 1] == 3) {
+                found = true;
+            }
+            if (nums[i] == 3) {
+                count++;
+            }
         }
-      }
+        return count == 3 && found == false;
+    }
 }
