8139e74a77c09f558075b75ea5295b5cc80ecbf4
vic233333 <ddbb520db@163.com>
2022-10-24 13:42:10 +0800

    fizzArray2

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -189,4 +189,12 @@ public class App {
         }
         return true;
     }
+
+    public String[] fizzArray2(int n) {
+        String[] arr = new String[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]  = i + "";
+        }
+        return arr;
+    }
 }
