6c2f73d310252f60b0f004d0b2d6b2491c3c39ee
vic233333 <ddbb520db@163.com>
2022-10-26 08:29:59 +0800

    sameEnds

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -376,4 +376,13 @@ public class App {
         }
         return true;
     }
+
+    public boolean sameEnds(int[] nums, int len) {
+        for (int i = 0, j = nums.length - len; i < len; i++, j++) {
+            if (nums[i] != nums[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
