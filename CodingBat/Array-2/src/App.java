cccb5c4dacac43c5b3c022b40d6f1a357b460d1e
vic233333 <ddbb520db@163.com>
2022-10-30 22:53:35 +0800

    either24 代码错误修正

--- CodingBat/Array-2/src/App.java
+++ CodingBat/Array-2/src/App.java
@@ -245,7 +245,7 @@ public class App {
             if (nums[i] == 2 && nums[i + 1] == 2) {
                 flag2 = true;
             }
-            if (nums[i] == 4 && nums[1 + 1] == 4) {
+            if (nums[i] == 4 && nums[i + 1] == 4) {
                 flag4 = true;
             }
         }
