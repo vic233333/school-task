3827a37e3a9817c90686ffbc63a66aa4077c780a
vic233333 <ddbb520db@163.com>
2022-10-31 13:44:20 +0800

    scores100

--- CodingBat/AP-1/src/App.java
+++ CodingBat/AP-1/src/App.java
@@ -11,4 +11,13 @@ public class App {
         }
         return true;
     }
+
+    public boolean scores100(int[] scores) {
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] == 100 && scores[i + 1] == 100) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
