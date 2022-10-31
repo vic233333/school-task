0cfdf010c86d002f1c29e6ccea0b6a2dea90dbbd
vic233333 <ddbb520db@163.com>
2022-10-31 14:48:41 +0800

    copyEndy

--- CodingBat/AP-1/src/App.java
+++ CodingBat/AP-1/src/App.java
@@ -135,6 +135,18 @@ public class App {
         return array;
     }
 
+    public int[] copyEndy(int[] nums, int count) {
+        int j = 0;
+        int[] arr = new int[count];
+        for (int i = 0; i < nums.length; i++) {
+            if ((j < arr.length) && ((nums[i] >= 0 && nums[i] <= 10) || (nums[i] >= 90 && nums[i] <= 100))) {
+                arr[j] = nums[i];
+                j++;
+            }
+        }
+        return arr;
+    }
+
     public int matchUp(String[] a, String[] b) {
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
