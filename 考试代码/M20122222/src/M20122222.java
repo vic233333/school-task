5751fbd753fbcd65ffc4f453891b4731a210cd45
vic233333 <ddbb520db@163.com>
2022-11-07 13:57:38 +0800

    array2DAllRows100B

--- 考试代码/M20122222/src/M20122222.java
+++ 考试代码/M20122222/src/M20122222.java
@@ -20,4 +20,22 @@ public class M20122222 {
         }
         return (flag_2 && (!flag_1 || !flag_4));
     }
+
+    public static boolean array2DAllRows100B(int[][] nums) {
+        if (nums.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < nums[0].length; j++) {
+                if (nums[i][j] >= 100) {
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
