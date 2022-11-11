1af1090dab2b0ac0575da10fa4528319a224874d
vic233333 <ddbb520db@163.com>
2022-11-11 11:16:40 +0800

    Magpie4 task2

--- APCS/LAB3/MagpieLab Materials/Magpie Lab Updates Sept 2014/MagpieActivityStarterCode/activity4/Magpie4.java
+++ APCS/LAB3/MagpieLab Materials/Magpie Lab Updates Sept 2014/MagpieActivityStarterCode/activity4/Magpie4.java
@@ -62,12 +62,17 @@ public class Magpie4
 		{
 			// Look for a two word (you <something> me)
 			// pattern
-			int psn = findKeyword(statement, "you", 0);
-
-			if (psn >= 0
-					&& findKeyword(statement, "me", psn) >= 0)
+			int psn_you = findKeyword(statement, "you", 0);
+			int psn_I = findKeyword(statement, "I", 0);
+			if (psn_you >= 0
+					&& findKeyword(statement, "me", psn_you) >= 0)
 			{
 				response = transformYouMeStatement(statement);
+			}
+			else if (psn_you >=0
+						&& findKeyword(statement, "you", psn_I) >= 0)
+			{
+				response = transformIYouStatement(statement);
 			}
 			else
 			{
@@ -141,6 +146,24 @@ public class Magpie4
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
 	
+	private String transformIYouStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		
+		int psnOfYou = findKeyword (statement, "I", 0);
+		int psnOfMe = findKeyword (statement, "you", psnOfYou + 1);
+		
+		String restOfStatement = statement.substring(psnOfYou + 1, psnOfMe).trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
 	
 
 	
