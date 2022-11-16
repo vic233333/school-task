55c52feac57b1c4a3b38541ae01066821fb421cb
vic233333 <ddbb520db@163.com>
2022-11-16 09:25:03 +0800

    activity1 Card

--- APCS/LAB3/AP_CompSciA_Elevens_Lab_Guide/Elevens/Activity Starter Code/Activity1 Starter Code/Card.java
+++ APCS/LAB3/AP_CompSciA_Elevens_Lab_Guide/Elevens/Activity Starter Code/Activity1 Starter Code/Card.java
@@ -32,7 +32,9 @@ public class Card {
 	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		suit = cardSuit;
+		rank = cardRank;
+		pointValue = cardPointValue;
 	}
 
 
@@ -41,7 +43,7 @@ public class Card {
 	 * @return this <code>Card's</code> suit.
 	 */
 	public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return suit;
    }
 
 	/**
@@ -49,7 +51,7 @@ public class Card {
 	 * @return this <code>Card's</code> rank.
 	 */
 	public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank;
 	}
 
    /**
@@ -57,7 +59,7 @@ public class Card {
 	 * @return this <code>Card's</code> point value.
 	 */
 	public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -67,7 +69,13 @@ public class Card {
 	 *         false otherwise.
 	 */
 	public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		if(otherCard.rank().equals(rank) &&
+				otherCard.suit().equals(suit) &&
+				otherCard.pointValue() == pointValue) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -82,6 +90,6 @@ public class Card {
 	 */
 	@Override
 	public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank + "of" + suit + "(point value = " + pointValue + ")";
 	}
 }
