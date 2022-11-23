09dbabf8667b934700dbe5f32ff631282d55c65d
vic233333 <ddbb520db@163.com>
2022-11-23 09:12:30 +0800

    debug

--- APCS/LAB3/AP_CompSciA_Elevens_Lab_Guide/Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
+++ APCS/LAB3/AP_CompSciA_Elevens_Lab_Guide/Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
@@ -12,7 +12,7 @@ public class DeckTester {
         String[] ranks1 = {"jack","queen","king","A","2","3","4","5","6","7","8","9","10"};
         String[] suits = {"spades","hearts","diamonds","clubs"};
         int[] pointValues = {10,10,10,11,2,3,4,5,6,7,8,9,10};
-        Deck d = new Deck(ranks,suits,pointValues);
+        Deck d = new Deck(ranks1,suits,pointValues);
         
         
         for(int i = 0;i < 5;i++){
