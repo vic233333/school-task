ba199d73306fe9d742d1b56fb3b95b4d7c0efa4f
vic233333 <ddbb520db@163.com>
2022-12-16 10:44:05 +0800

    Advance done

--- APCS/FRQ/x2005S/A2/Advance.java
+++ APCS/FRQ/x2005S/A2/Advance.java
@@ -1,6 +1,18 @@
 /*** Part (a) ***/
 
-public class Advance 
-{
-  
+public class Advance extends Ticket {
+	private int days;
+
+	public Advance(int d) {
+		days = d;
+	}
+
+
+	public double getPrice() {
+		if (days <= 9) {
+			return 40.0;
+		} else {
+			return 30.0;
+		}
+	}
 }
