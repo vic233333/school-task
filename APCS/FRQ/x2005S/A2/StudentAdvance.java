ab6cecff4efaabf0c587d30e823ce71bc89cbf16
vic233333 <ddbb520db@163.com>
2022-12-16 11:26:13 +0800

    StudentAdvance done

--- APCS/FRQ/x2005S/A2/StudentAdvance.java
+++ APCS/FRQ/x2005S/A2/StudentAdvance.java
@@ -1,6 +1,17 @@
 /*** Part (b) ***/
 
-public class StudentAdvance 
-{
- 
+public class StudentAdvance extends Advance {
+
+	public StudentAdvance(int d) {
+		super(d);
+	}
+
+	public double getPrice() {
+		return super.getPrice() / 2;
+	}
+
+
+	public String toString() {
+		return super.toString() + "\n(student ID required)";
+	}
 }
