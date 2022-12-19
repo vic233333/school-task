6d440d52239452b5bc98011d3d3050c5f50b8b1d
vic233333 <ddbb520db@163.com>
2022-12-19 15:48:14 +0800

    2006 A2

--- APCS/FRQ/x2006S/A2/TaxableItem.java
+++ APCS/FRQ/x2006S/A2/TaxableItem.java
@@ -1,20 +1,16 @@
-public abstract class TaxableItem implements Item
-{
-  private double taxRate;
+public abstract class TaxableItem implements Item {
+	private double taxRate;
 
+	public abstract double getListPrice();
 
-  public abstract double getListPrice();
+	public TaxableItem(double rate) {
+		taxRate = rate;
+	}
 
+	/*** Part (a) ***/
 
-  public TaxableItem(double rate)
-  {  taxRate = rate;  }
-
-
-  /*** Part (a) ***/
-
-  // returns the price of the item including the tax
-  public double purchasePrice()
-  {
-   
-  }
+	// returns the price of the item including the tax
+	public double purchasePrice() {
+		return getListPrice()*(1+ taxRate);
+	}
 }
--- APCS/FRQ/x2006S/A2/Vehicle.java
+++ APCS/FRQ/x2006S/A2/Vehicle.java
@@ -1,6 +1,23 @@
-  /*** Part (b) ***/
+/*** Part (b) ***/
 
-public class Vehicle 
-{
-  
+public class Vehicle extends TaxableItem{
+
+	private double cost;
+	private double markup;
+
+	public Vehicle(double c1, double m1, double rate) {
+		super(rate);
+		cost = c1;
+		markup = m1;
+	}
+
+	public double getListPrice() {
+		double listPrice = cost + markup;
+		return listPrice;
+	}
+
+	public void changeMarkup(double m2) {
+		markup = m2;
+	}
+
 }
