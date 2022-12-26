036b4671f88064657fc30a6e574779d0d0053afe
vic233333 <ddbb520db@163.com>
2022-12-26 17:24:11 +0800

    2015 A2 method2

--- APCS/FRQ/x2015S/A2/HiddenWord_2.java
+++ APCS/FRQ/x2015S/A2/HiddenWord_2.java
@@ -1,24 +1,22 @@
 /**
- * 方法一
+ * 方法二
  */
-public class HiddenWord {
+public class HiddenWord_2 {
 	private String word;
 
-	public HiddenWord(String w1) {
+	public HiddenWord_2(String w1) {
 		word = w1;
 	}
 
 	public String getHint(String guess) {
-		String res = "";
+		String res = guess;
 		for (int i = 0; i < guess.length(); i++) {
-			if (guess.substring(i, i + 1).equals(word.substring(i, i + 1))) {
-				// in the same position
-				res += word.substring(i, i + 1);
-			} else if (word.indexOf(guess.substring(i, i + 1)) != -1) {
-				// 排除第一种情况以后,在其他的位置
-				res += "+";
-			} else {// not in the hidden word
-				res += "*";
+			if (!guess.substring(i, i + 1).equals(word.substring(i, i + 1))) {
+				if (word.indexOf(guess.substring(i, i + 1)) != -1) {
+					res = res.substring(0, i) + '+' + res.substring(i + 1);
+				} else {
+					res = res.substring(0, i) + "*" + res.substring(i + 1);
+				}
 			}
 		}
 		return res;
