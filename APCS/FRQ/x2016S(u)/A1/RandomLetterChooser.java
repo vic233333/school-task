c932eadd59820b5a8868620a84ab7548204bb5a5
vic233333 <ddbb520db@163.com>
2023-02-17 10:48:46 +0800

    2016 1 (b) done

--- APCS/FRQ/x2016S(u)/A1/RandomLetterChooser.java
+++ APCS/FRQ/x2016S(u)/A1/RandomLetterChooser.java
@@ -1,22 +1,18 @@
-public class RandomLetterChooser extends RandomStringChooser
-{
-  /* Part (b) */
+public class RandomLetterChooser extends RandomStringChooser {
+	/* Part (b) */
 
-  public RandomLetterChooser(String str)
-  {
-    
-  }
+	public RandomLetterChooser(String str) {
+		super(getSingleLetters(str));
+	}
 
-  public static String[] getSingleLetters(String str)
-  {
-    String[] letters = new String[str.length()];
+	public static String[] getSingleLetters(String str) {
+		String[] letters = new String[str.length()];
 
-    for (int i = 0; i < str.length(); i++)
-      letters[i] = str.substring(i, i+1);
+		for (int i = 0; i < str.length(); i++)
+			letters[i] = str.substring(i, i + 1);
 
-    return letters;
-  }
-  
+		return letters;
+	}
 
 
 }
