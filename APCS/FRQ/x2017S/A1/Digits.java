9aee5a713e2b9f9970a9d59855e64cbc2ab687d4
vic233333 <ddbb520db@163.com>
2023-02-24 10:24:12 +0800

    2017 A1 done

--- APCS/FRQ/x2017S/A1/Digits.java
+++ APCS/FRQ/x2017S/A1/Digits.java
@@ -1,55 +1,59 @@
 import java.util.ArrayList;
 
-public class Digits
-{
-  /** The list of digits from the number used to construct this object.
-   *  The digits appear in the list in the same order in which they appear in the original number.
-   */
-  private ArrayList<Integer> digitList;
+public class Digits {
+	/**
+	 * The list of digits from the number used to construct this object.
+	 * The digits appear in the list in the same order in which they appear in the original number.
+	 */
+	private ArrayList<Integer> digitList;
 
-  /**************** Part (a) ****************/
+	/**************** Part (a) ****************/
 
-  /** Constructs a Digits object that represents num.
-   *  Precondition: num >= 0
-   */
+	/**
+	 * Constructs a Digits object that represents num.
+	 * Precondition: num >= 0
+	 */
+	public Digits(int num) {
+		digitList = new ArrayList<Integer>();
+		if (num == 0) {
+			digitList.add(0);
+		}
+		while (num > 0) {
+			int dig = num % 10;
+			digitList.add(0, dig);
+			num = num / 10;
+		}
+	}
 
-  public Digits(int num)
-  {
-   
-      
-  }
+	/*  int a = Integer.parseInt(str);
+	 * int b = Integer.valueOf(str).intValue()
+	 */
 
-  /*  int a = Integer.parseInt(str);
-   * int b = Integer.valueOf(str).intValue()
-   */
+	public String toString() {
+		return digitList.toString();
+	}
 
- public String toString()
-  {
-    return digitList.toString();
-  }
+	/**************** Part (b) ****************/
 
-  /**************** Part (b) ****************/
+	/**
+	 * Returns true if the digits in this Digits object are in strictly increasing order;
+	 * false otherwise.
+	 */
+	public boolean isStrictlyIncreasing() {
 
-  /** Returns true if the digits in this Digits object are in strictly increasing order;
-   *  false otherwise.
-   */
-  public boolean isStrictlyIncreasing()
-  {
-    
-  }
+	}
 
-  public static void main(String[] args)
-  {
-    Digits d1 = new Digits(15704);
-    System.out.println(d1);
+	public static void main(String[] args) {
+		Digits d1 = new Digits(15704);
+		System.out.println(d1);
 
-    Digits d2 = new Digits(0);
-    System.out.println(d2);
+		Digits d2 = new Digits(0);
+		System.out.println(d2);
 
-    System.out.println("7 shouble be true,your result is "+(new Digits(7)).isStrictlyIncreasing()); // true
-    System.out.println("1356 shouble be true,your result is "+(new Digits(1356)).isStrictlyIncreasing()); // true
-    System.out.println("1336 shouble be false,your result is "+(new Digits(1336)).isStrictlyIncreasing()); // false
-    System.out.println("1536 shouble be false,your result is "+(new Digits(1536)).isStrictlyIncreasing()); // false
-    System.out.println("65310 shouble be false,your result is "+(new Digits(65310)).isStrictlyIncreasing()); // false
-  }
+		System.out.println("7 shouble be true,your result is " + (new Digits(7)).isStrictlyIncreasing()); // true
+		System.out.println("1356 shouble be true,your result is " + (new Digits(1356)).isStrictlyIncreasing()); // true
+		System.out.println("1336 shouble be false,your result is " + (new Digits(1336)).isStrictlyIncreasing()); // false
+		System.out.println("1536 shouble be false,your result is " + (new Digits(1536)).isStrictlyIncreasing()); // false
+		System.out.println("65310 shouble be false,your result is " + (new Digits(65310)).isStrictlyIncreasing()); // false
+	}
 }
