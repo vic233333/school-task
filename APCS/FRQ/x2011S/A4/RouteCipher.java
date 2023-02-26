70f1f1bf70f1a7010a594c4f9b458574bb654100
vic233333 <ddbb520db@163.com>
2023-02-27 03:38:25 +0800

    2011 A4 a done

--- APCS/FRQ/x2011S/A4/RouteCipher.java
+++ APCS/FRQ/x2011S/A4/RouteCipher.java
@@ -1,60 +1,76 @@
-public class RouteCipher
-{
-  /** A two-dimensional array of single-character strings, instantiated in the constructor */
-  private String[][] letterBlock;
+public class RouteCipher {
+	/**
+	 * A two-dimensional array of single-character strings, instantiated in the
+	 * constructor
+	 */
+	private String[][] letterBlock;
 
-  /** The number of rows of letterBlock, set by the constructor */
-  private int numRows;
+	/** The number of rows of letterBlock, set by the constructor */
+	private int numRows;
 
-  /** The number of columns of letterBlock, set by the constructor */
-  private int numCols;
+	/** The number of columns of letterBlock, set by the constructor */
+	private int numCols;
 
-  // Part (a)
+	// Part (a)
 
-  /** Places a string into letterBlock in row-major order.
-   *  @param str the string to be processed
-   *  Postcondition:
-   *    if str.length() < numRows * numCols, "A" is placed in each unfilled cell
-   *    if str.length() > numRows * numCols, trailing characters are ignored
-   */
-  private void fillBlock(String str)
-  {
-    
-  }
+	/**
+	 * Places a string into letterBlock in row-major order.
+	 * 
+	 * @param str the string to be processed
+	 *            Postcondition:
+	 *            if str.length() < numRows * numCols, "A" is placed in each
+	 *            unfilled cell
+	 *            if str.length() > numRows * numCols, trailing characters are
+	 *            ignored
+	 */
+	private void fillBlock(String str) {
+		int i = 0;
+		for (int r = 0; r < letterBlock.length; r++) {
+			for (int c = 0; c < letterBlock[0].length; c++) {
+				if (i < str.length()) {
+					letterBlock[r][c] = str.substring(i, i + 1);
+					i++;
+				} else {
+					letterBlock[r][c] = "A";
+				}
+			}
+		}
+	}
 
-  /** Extracts encrypted string from letterBlock in column-major order.
-   *  Precondition: letterBlock has been filled
-   *  @return the encrypted string from letterBlock
-   */
-  private String encryptBlock()
-  {
-    String str = "";
+	/**
+	 * Extracts encrypted string from letterBlock in column-major order.
+	 * Precondition: letterBlock has been filled
+	 * 
+	 * @return the encrypted string from letterBlock
+	 */
+	private String encryptBlock() {
+		String str = "";
 
-    for (int c = 0; c < numCols; c++)
-      for (int r = 0; r < numRows; r++)
-        str += letterBlock[r][c];
+		for (int c = 0; c < numCols; c++)
+			for (int r = 0; r < numRows; r++)
+				str += letterBlock[r][c];
 
-    return str;
-  }
+		return str;
+	}
 
-  // Part (b)
+	// Part (b)
 
-  /** Encrypts a message.
-   * @param message the string to be encrypted
-   * @return the encrypted message;
-   *         if message is the empty string, returns the empty string
-   */
-  public String encryptMessage(String message)
-  {
-    
-  }
+	/**
+	 * Encrypts a message.
+	 * 
+	 * @param message the string to be encrypted
+	 * @return the encrypted message;
+	 *         if message is the empty string, returns the empty string
+	 */
+	public String encryptMessage(String message) {
 
-  public static void main (String[] args)
-  {
-    RouteCipher c = new RouteCipher();
-    c.numRows = 2;
-    c.numCols = 3;
-    c.letterBlock = new String[c.numRows][c.numCols];
-    System.out.println("[" + c.encryptMessage("Meet at midnight") +"]");
-  }
+	}
+
+	public static void main(String[] args) {
+		RouteCipher c = new RouteCipher();
+		c.numRows = 2;
+		c.numCols = 3;
+		c.letterBlock = new String[c.numRows][c.numCols];
+		System.out.println("[" + c.encryptMessage("Meet at midnight") + "]");
+	}
 }
