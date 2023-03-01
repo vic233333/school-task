46224ed029c9432a612c5b04dfca7e83a6e2bab7
vic233333 <ddbb520db@163.com>
2023-03-01 08:08:22 +0800

    2018 A2 done

--- APCS/FRQ/x2018S/x2018S/A2/WordPair.java
+++ APCS/FRQ/x2018S/x2018S/A2/WordPair.java
@@ -1,19 +1,30 @@
-public class WordPair
-{
-  private String first, second;
+public class WordPair {
+	private String first, second;
 
-  /** Constructs a WordPair object. */
-  public WordPair(String first, String second) { this.first = first; this.second = second; }
+	/**
+	 * Constructs a WordPair object.
+	 */
+	public WordPair(String first, String second) {
+		this.first = first;
+		this.second = second;
+	}
 
-  /** Returns the first string of this WordPair object. */
-  public String getFirst() { return first; }
+	/**
+	 * Returns the first string of this WordPair object.
+	 */
+	public String getFirst() {
+		return first;
+	}
 
 
-  /** Returns the second string of this WordPair object. */
-  public String getSecond() { return second; }
+	/**
+	 * Returns the second string of this WordPair object.
+	 */
+	public String getSecond() {
+		return second;
+	}
 
-  public String toString()
-  {
-    return "(" + first + ", " + second + ")";
-  }
+	public String toString() {
+		return "(" + first + ", " + second + ")";
+	}
 }
--- APCS/FRQ/x2018S/x2018S/A2/WordPairList.java
+++ APCS/FRQ/x2018S/x2018S/A2/WordPairList.java
@@ -1,45 +1,58 @@
 import java.util.ArrayList;
 
-public class WordPairList
-{
-  /** The list of word pairs, initialized by the constructor. */
-  private ArrayList<WordPair> allPairs;
+public class WordPairList {
+	/**
+	 * The list of word pairs, initialized by the constructor.
+	 */
+	private ArrayList<WordPair> allPairs;
 
-  /********************** Part (a) *************************/
+	/********************** Part (a) *************************/
 
-  /** Constructs a WordPairList object as described in part (a).
-   *  Precondition: words.length >= 2
-   */
-  public WordPairList(String[] words)
-  {
-    
-  }
+	/**
+	 * Constructs a WordPairList object as described in part (a).
+	 * Precondition: words.length >= 2
+	 */
+	public WordPairList(String[] words) {
+		allPairs = new ArrayList<WordPair>();
+		for (int i = 0; i < words.length; i++) {
+			for (int j = i + 1; j < words.length; j++) {
+				allPairs.add(new WordPair(words[i], words[j]));
+			}
+		}
+	}
 
-  /********************** Part (b) *************************/
+	/********************** Part (b) *************************/
 
-  /** Returns the number of matches as described in part (b).
-   */
-  public int numMatches()
-  {
-    
-  }
+	/**
+	 * Returns the number of matches as described in part (b).
+	 */
+	public int numMatches() {
+		int count = 0;
+		for (int i = 0; i < allPairs.size(); i++) {
+			if (allPairs.get(i).getFirst().equals(allPairs.get(i).getSecond())) {
+				count++;
+			}
+		}
+		return count;
+	}
 
-  /********************** Test *************************/
+	/********************** Test *************************/
 
-  public String toString() { return allPairs.toString(); }
+	public String toString() {
+		return allPairs.toString();
+	}
 
-  public static void main(String[] args)
-  {
-    String[] wordNums = {"one", "two", "three"};
-    WordPairList exampleOne = new WordPairList(wordNums);
-    System.out.println(exampleOne);
+	public static void main(String[] args) {
+		String[] wordNums = {"one", "two", "three"};
+		WordPairList exampleOne = new WordPairList(wordNums);
+		System.out.println(exampleOne);
 
-    String[] phrase = {"the", "more", "the", "merrier"};
-    WordPairList exampleTwo = new WordPairList(phrase);
-    System.out.println(exampleTwo);
+		String[] phrase = {"the", "more", "the", "merrier"};
+		WordPairList exampleTwo = new WordPairList(phrase);
+		System.out.println(exampleTwo);
 
-    String[] moreWords = {"the", "red", "fox", "the", "red"};
-    WordPairList exampleThree = new WordPairList(moreWords);
-    System.out.println(exampleThree.numMatches());
-  }
+		String[] moreWords = {"the", "red", "fox", "the", "red"};
+		WordPairList exampleThree = new WordPairList(moreWords);
+		System.out.println(exampleThree.numMatches());
+	}
 }
