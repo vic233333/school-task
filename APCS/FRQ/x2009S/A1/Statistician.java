cd6967faa6034f4041f54b87ea9cdf305dd41af8
vic233333 <ddbb520db@163.com>
2023-03-03 10:27:49 +0800

    2009 A1 b done

--- APCS/FRQ/x2009S/A1/Statistician.java
+++ APCS/FRQ/x2009S/A1/Statistician.java
@@ -1,46 +1,63 @@
 import java.util.Arrays;
 
-public class Statistician
-{
-  // Part (a)
+public class Statistician {
+	// Part (a)
 
-  /** Returns an array of the values obtained by tossing a number cube numTosses times.
-   *  @param cube a NumberCube
-   *  @param numTosses the number of tosses to be recorded
-   *         Precondition: numTosses > 0
-   *  @return an array of numTosses values
-   */
-  public static int[] getCubeTosses(NumberCube cube, int numTosses)
-  {
-   
-  }
+	/**
+	 * Returns an array of the values obtained by tossing a number cube numTosses times.
+	 *
+	 * @param cube      a NumberCube
+	 * @param numTosses the number of tosses to be recorded
+	 *                  Precondition: numTosses > 0
+	 * @return an array of numTosses values
+	 */
+	public static int[] getCubeTosses(NumberCube cube, int numTosses) {
 
-  // *************************************************
+	}
 
-  // Part (b)
+	// *************************************************
 
-  /** Returns the starting index of a longest run of two or more consecutive repeated values
-   *  in the array values.
-   *  @param values an array of integer values representing a series of number cube tosses
-   *         Precondition: values.length > 0
-   *  @return the starting index of a run of maximum size;
-   *          -1 if there is no run
-   */
-  public static int getLongestRun(int[] values)
-  {
-    
-  }
+	// Part (b)
 
-  // *************************************************
+	/**
+	 * Returns the starting index of a longest run of two or more consecutive repeated values
+	 * in the array values.
+	 *
+	 * @param values an array of integer values representing a series of number cube tosses
+	 *               Precondition: values.length > 0
+	 * @return the starting index of a run of maximum size;
+	 * -1 if there is no run
+	 */
+	public static int getLongestRun(int[] values) {
+		int max = 0;
+		int maxi = 0;
+		for (int i = 0; i < values.length; i++) {
+			int j = i + 1;
+			int count = 0;
+			while (j < values.length && values[j] == values[i]) {
+				j++;
+				count++;
+			}
+			if (count > max) {
+				maxi = i;
+				max = count;
+			}
+		}
+		if (max == 0) {
+			return -1;
+		} else {
+			return maxi;
+		}
+	}
 
-  public static void main(String[] args)
-  {
-    NumberCube cube = new NumberCube();
-    for (int count = 1; count < 10; count++)
-    {
-      int[] values = getCubeTosses(cube, 18);
-      int maxRunStart = getLongestRun(values);
-      System.out.println(Arrays.toString(values) + " ==> " + maxRunStart);
-    }
-  }
+	// *************************************************
+
+	public static void main(String[] args) {
+		NumberCube cube = new NumberCube();
+		for (int count = 1; count < 10; count++) {
+			int[] values = getCubeTosses(cube, 18);
+			int maxRunStart = getLongestRun(values);
+			System.out.println(Arrays.toString(values) + " ==> " + maxRunStart);
+		}
+	}
 }
