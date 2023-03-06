f55e0a990930385d05cec8ca8a3f911390b739a1
vic233333 <ddbb520db@163.com>
2023-03-06 13:54:15 +0800

    2011 A1 b done

--- APCS/FRQ/x2011S/A1/Sound.java
+++ APCS/FRQ/x2011S/A1/Sound.java
@@ -1,48 +1,56 @@
 import java.util.Arrays;
 
-public class Sound
-{
-  /** the array of values in this sound; guaranteed not to be null */
-  private int[] samples;
+public class Sound {
+	/** the array of values in this sound; guaranteed not to be null */
+	private int[] samples;
 
-  // Part (a)
+	// Part (a)
 
-  /** Changes those values in this sound that have an amplitude greater than limit.
-   *  Values greater than limit are changed to limit.
-   *  Values less than -limit are changed to -limit.
-   *  @param limit the amplitude limit
-   *         Precondition: limit >= 0
-   *  @return the number of values in this sound that this method changed
-   */
-  public int limitAmplitude(int limit)
-  {  
-    
-  }
+	/**
+	 * Changes those values in this sound that have an amplitude greater than limit.
+	 * Values greater than limit are changed to limit.
+	 * Values less than -limit are changed to -limit.
+	 * 
+	 * @param limit the amplitude limit
+	 *              Precondition: limit >= 0
+	 * @return the number of values in this sound that this method changed
+	 */
+	public int limitAmplitude(int limit) {
 
-  // Part (b)
+	}
 
-  /** Removes all silence from the beginning of this sound.
-   *  Silence is represented by a value of 0.
-   *  Precondition: samples contains at least one nonzero value
-   *  Postcondition: the length of samples reflects the removal of starting silence
-   */
-  public void trimSilenceFromBeginning()
-  {
-    
-  }
+	// Part (b)
 
-  public static void main(String[] args)
-  {
-    Sound sound = new Sound();
-    sound.samples = new int[] {40, 2532, 17, -2300, -17, -4000, 2000, 1048,
-                                     -420, 33, 15, -32, 2030, 3223};
-    int numChanges = sound.limitAmplitude(2000);
-    System.out.println(numChanges);
-    System.out.println(Arrays.toString(sound.samples));
+	/**
+	 * Removes all silence from the beginning of this sound.
+	 * Silence is represented by a value of 0.
+	 * Precondition: samples contains at least one nonzero value
+	 * Postcondition: the length of samples reflects the removal of starting silence
+	 */
+	public void trimSilenceFromBeginning() {
+		int j = 0;
+		while(j<samples.length&& samples[j] ==0){
+			j++;
+		}
+		int[] arr = new int[samples.length-j];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = samples[j];
+			// j ++;
+		}
+		samples = arr;
+	}
 
-    sound.samples = new int[] {0, 0, 0, 0, -14, 0, -35, -39, 0,
-                                      -7, 16, 32, 37, 29, 0, 0};
-    sound.trimSilenceFromBeginning();
-    System.out.println(Arrays.toString(sound.samples));
-  }
+	public static void main(String[] args) {
+		Sound sound = new Sound();
+		sound.samples = new int[] { 40, 2532, 17, -2300, -17, -4000, 2000, 1048,
+				-420, 33, 15, -32, 2030, 3223 };
+		int numChanges = sound.limitAmplitude(2000);
+		System.out.println(numChanges);
+		System.out.println(Arrays.toString(sound.samples));
+
+		sound.samples = new int[] { 0, 0, 0, 0, -14, 0, -35, -39, 0,
+				-7, 16, 32, 37, 29, 0, 0 };
+		sound.trimSilenceFromBeginning();
+		System.out.println(Arrays.toString(sound.samples));
+	}
 }
