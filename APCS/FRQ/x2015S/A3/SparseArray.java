ecfdd695c5e6d8a6e60db2184ce32f8b3a156b1d
vic233333 <ddbb520db@163.com>
2023-03-27 16:11:46 +0800

    2015 A3 done

--- APCS/FRQ/x2015S/A3/SparseArray.java
+++ APCS/FRQ/x2015S/A3/SparseArray.java
@@ -1,78 +1,95 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class SparseArray
-{
-  /** The number of rows and columns in the sparse array. */
-  private int numRows;
-  private int numCols;
-  
-  /**
-   * The list of entries representing the non-zero elements of the sparse array. Entries are stored in the
-   * list in no particular order. Each non-zero element is represented by exactly one entry in the list.
-   */
-  private List<SparseArrayEntry> entries;
+public class SparseArray {
+	/**
+	 * The number of rows and columns in the sparse array.
+	 */
+	private int numRows;
+	private int numCols;
 
-  /** Constructs and empty sparse array. */  
-  public SparseArray()
-  {
-    entries = new ArrayList<SparseArrayEntry>();
-  }
+	/**
+	 * The list of entries representing the non-zero elements of the sparse array. Entries are stored in the
+	 * list in no particular order. Each non-zero element is represented by exactly one entry in the list.
+	 */
+	private List<SparseArrayEntry> entries;
 
-  /** Returns the number of rows in the sparse array. */
-  public int getNumRows()
-  { return numRows; }
+	/**
+	 * Constructs and empty sparse array.
+	 */
+	public SparseArray() {
+		entries = new ArrayList<SparseArrayEntry>();
+	}
 
-  /** Returns the number of columns in the sparse array. */
-  public int getNumCols()
-  { return numCols; }
+	/**
+	 * Returns the number of rows in the sparse array.
+	 */
+	public int getNumRows() {
+		return numRows;
+	}
 
-  // Part (a)
-  
-  public int getValueAt(int row, int col)
-  {
-    
-  }
+	/**
+	 * Returns the number of columns in the sparse array.
+	 */
+	public int getNumCols() {
+		return numCols;
+	}
 
-  // Part (b)
+	// Part (a)
 
-  public void removeColumn(int col)
-  {
-    
-  }
+	public int getValueAt(int row, int col) {
+		for (int i = 0; i < entries.size(); i++) {
+			if (entries.get(i).getRow() == row && entries.get(i).getCol() == col) {
+				return entries.get(i).getValue();
+			}
+		}
+		return 0;
+	}
+
+	// Part (b)
+
+	public void removeColumn(int col) {
+		for (int i = 0; i < entries.size(); i++) {
+			if (entries.get(i).getCol() == col) {
+				entries.remove(i);
+				i--;
+			} else if (entries.get(i).getCol() > col) {
+				SparseArrayEntry s = new SparseArrayEntry(entries.get(i).getRow(), entries.get(i).getCol() - 1, entries.get(i).getValue());
+				entries.set(i, s);
+			}
+		}
+		numCols--;
+	}
 
 
+	/******************************************************************************/
 
-  /******************************************************************************/
+	public SparseArray(int rows, int cols) {
+		entries = new ArrayList<SparseArrayEntry>();
+		numRows = rows;
+		numCols = cols;
+	}
 
-  public SparseArray(int rows, int cols)
-  {
-    entries = new ArrayList<SparseArrayEntry>();
-    numRows = rows;
-    numCols = cols;
-  }
+	public void add(int row, int col, int value) {
+		entries.add(new SparseArrayEntry(row, col, value));
+	}
 
-  public void add(int row, int col, int value)
-  { entries.add(new SparseArrayEntry(row, col, value)); }
-  
-  public String toString()
-  {
-    return getNumRows() + " " + getNumCols() + "\n" + entries;
-  }
-  
-  public static void main(String[] args)
-  {
-    SparseArray sparse = new SparseArray(6, 5);
-    sparse.add(1, 4, 4);
-    sparse.add(2, 0, 1);
-    sparse.add(3, 1, -9);
-    sparse.add(1, 1, 5);
-    
-    System.out.println(sparse.getValueAt(3, 1));
-    System.out.println(sparse.getValueAt(3, 3));
+	public String toString() {
+		return getNumRows() + " " + getNumCols() + "\n" + entries;
+	}
 
-    System.out.println(sparse);
-    sparse.removeColumn(1);    
-    System.out.println(sparse);
-  }  
+	public static void main(String[] args) {
+		SparseArray sparse = new SparseArray(6, 5);
+		sparse.add(1, 4, 4);
+		sparse.add(2, 0, 1);
+		sparse.add(3, 1, -9);
+		sparse.add(1, 1, 5);
+
+		System.out.println(sparse.getValueAt(3, 1));
+		System.out.println(sparse.getValueAt(3, 3));
+
+		System.out.println(sparse);
+		sparse.removeColumn(1);
+		System.out.println(sparse);
+	}
 }
