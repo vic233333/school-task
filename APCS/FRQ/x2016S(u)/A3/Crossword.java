95a34a8b393ca70be120b776751e016c4b20dbbd
vic233333 <ddbb520db@163.com>
2023-03-27 14:26:27 +0800

    2016 A3 done

--- APCS/FRQ/x2016S(u)/A3/Crossword.java
+++ APCS/FRQ/x2016S(u)/A3/Crossword.java
@@ -1,20 +1,39 @@
-public class Crossword
-{
-  private Square[][] puzzle;
+public class Crossword {
+	private Square[][] puzzle;
 
-  /* Part (b) */
+	/* Part (b) */
 
-  public Crossword(boolean[][] blackSquares)
-  {
-    
-  }
+	public Crossword(boolean[][] blackSquares) {
+		puzzle = new Square[blackSquares.length][blackSquares[0].length];
+		int count = 1;
+		for (int r = 0; r < puzzle.length; r++) {
+			for (int c = 0; c < puzzle[0].length; c++) {
+				if (blackSquares[r][c] == true) {
+					puzzle[r][c] = new Square(true, 0);
+				} else if (toBeLabeled(r, c, blackSquares) == false) {
+					puzzle[r][c] = new Square(false, 0);
+				} else {
+					puzzle[r][c] = new Square(false, count);
+					count++;
+				}
+			}
+		}
+	}
+	/* Part (a) */
 
-  /* Part (a) */
+	private boolean toBeLabeled(int r, int c, boolean[][] blackSquares) {
+		if (blackSquares[r][c] == true) {
+			return false;
+		} else {
+			if (r == 0 || c == 0) {
+				return true;
+			} else {
+				return blackSquares[r - 1][c] == true || blackSquares[r][c - 1] == true;
+			}
+		}
+	}
 
-  private boolean toBeLabeled(int r, int c, boolean[][] blackSquares)
-  {
-   
-  }
-
-  public Square getSquare(int r, int c) { return puzzle[r][c]; }
+	public Square getSquare(int r, int c) {
+		return puzzle[r][c];
+	}
 }
