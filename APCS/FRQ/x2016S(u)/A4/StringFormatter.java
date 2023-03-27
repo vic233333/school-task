d0382f391a350c5f8a5c0f68aee8c6c558f26bf8
vic233333 <ddbb520db@163.com>
2023-03-27 14:39:15 +0800

    2016 A4 done

--- APCS/FRQ/x2016S(u)/A4/StringFormatter.java
+++ APCS/FRQ/x2016S(u)/A4/StringFormatter.java
@@ -1,39 +1,52 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class StringFormatter
-{
-  /* Part (a) */
+public class StringFormatter {
+	/* Part (a) */
 
-  public static int totalLetters(List<String> wordList)
-  {
-    int count = 0;
+	public static int totalLetters(List<String> wordList) {
+		int total = 0;
+		for (int i = 0; i < wordList.size(); i++) {
+			total += wordList.get(i).length();
+		}
+		return total;
+	}
 
-    for (String word : wordList)
-      count += word.length();
+	/* Part (b) */
 
-    return count;
-  }
+	public static int basicGapWidth(List<String> wordList,
+									int formattedLen) {
+		return (formattedLen - totalLetters(wordList)) / (wordList.size() - 1);
+	}
 
-  /* Part (b) */
+	/***********/
 
-  public static int basicGapWidth(List<String> wordList,
-                                  int formattedLen)
-  {
-    
-  }
-  /***********/
+	public static int leftoverSpaces(List<String> wordList,
+									 int formattedLen) {
+		return formattedLen - totalLetters(wordList) -
+				basicGapWidth(wordList, formattedLen) * (wordList.size() - 1);
+	}
 
-  public static int leftoverSpaces(List<String> wordList,
-                                                int formattedLen)
-  {
-    return formattedLen - totalLetters(wordList) -
-                 basicGapWidth(wordList, formattedLen) * (wordList.size() - 1);
-  }
+	/* Part (c) */
 
-  /* Part (c) */
-
-  public static String format(List<String> wordList, int formattedLen)
-  {
-    
+	public static String format(List<String> wordList, int formattedLen) {
+		int gapLen = basicGapWidth(wordList, formattedLen);
+		int leftoversLen = leftoverSpaces(wordList, formattedLen);
+		String sum = "";
+		String gap = "";
+		for (int i = 0; i < gapLen; i++) {
+			gap = gap + " ";
+		}
+		for (int i = 0; i < wordList.size(); i++) {
+			sum = sum + wordList.get(i);
+			if (i < wordList.size() - 1) {
+				sum += gap;
+			}
+			if (leftoversLen > 0) {
+				sum = sum + " ";
+				leftoversLen--;
+			}
+		}
+		return sum;
+	}
 }
