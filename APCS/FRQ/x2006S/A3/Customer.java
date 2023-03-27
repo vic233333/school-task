eadd89845bdc831e37ce5c6f3838a1cd9d383023
vic233333 <ddbb520db@163.com>
2023-03-27 14:34:18 +0800

    2006 A3 done

--- APCS/FRQ/x2006S/A3/Customer.java
+++ APCS/FRQ/x2006S/A3/Customer.java
@@ -1,32 +1,37 @@
-public class Customer
-{
-  private String name;
-  private int idNum;
+public class Customer {
+	private String name;
+	private int idNum;
 
-  // constructs a Customer with given name and ID number
-  public Customer(String name, int idNum)
-  { this.name = name; this.idNum = idNum; }
+	// constructs a Customer with given name and ID number
+	public Customer(String name, int idNum) {
+		this.name = name;
+		this.idNum = idNum;
+	}
 
-  // returns the customer's name
-  public String getName()
-  { return name; }
+	// returns the customer's name
+	public String getName() {
+		return name;
+	}
 
-  // returns the customer's id
-  public int getID()
-  { return idNum; }
+	// returns the customer's id
+	public int getID() {
+		return idNum;
+	}
 
-  /*** Part (a) ***/
+	/*** Part (a) ***/
 
-  // returns 0 when this customer is equal to other;
-  //   a positive integer when this customer is greater than other;
-  //   a negative integer when this customer is less than other
-  public int compareCustomer(Customer other)
-  {
-    
-  }
+	// returns 0 when this customer is equal to other;
+	//   a positive integer when this customer is greater than other;
+	//   a negative integer when this customer is less than other
+	public int compareCustomer(Customer other) {
+		if (name.compareTo(other.name) == 0) {
+			return idNum - other.idNum;
+		} else {
+			return name.compareTo(other.name);
+		}
+	}
 
-  public String toString()
-  {
-    return name + " " + idNum;
-  }
+	public String toString() {
+		return name + " " + idNum;
+	}
 }
