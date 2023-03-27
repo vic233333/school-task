a40b7c13848938754e923111640c34630e6b1ed2
vic233333 <ddbb520db@163.com>
2023-03-27 15:53:37 +0800

    2016 A2 done

--- APCS/FRQ/x2016S(u)/A2/LogMessage.java
+++ APCS/FRQ/x2016S(u)/A2/LogMessage.java
@@ -1,31 +1,50 @@
-public class LogMessage
-{
-  private String machineId;
-  private String description;
+public class LogMessage {
+	private String machineId;
+	private String description;
 
-  /* Part (a) */
+	/* Part (a) */
 
-  public LogMessage(String message)
-  {
-   
-  }
+	public LogMessage(String message) {
+		int i = message.indexOf(":");
+		machineId = message.substring(0, i);
+		description = message.substring(i + 1);
+	}
 
-  /* Part (b) */
+	/* Part (b) */
+	/* method 1 */
+	public boolean containsWord(String keyword) {
+		int i = description.indexOf(keyword);
+		while (i >= 0) {
+			String before = "";
+			String after = " ";
+			if (i > 0) {
+				before = description.substring(i - 1, i);
+			}
+			if (i + keyword.length() < description.length()) {
+				after = description.substring(i + keyword.length(), i + keyword.length() + 1);
+			}
+			if (before.equals("") && after.equals("")) {
+				return true;
+			}
+			i = description.indexOf(keyword, i + 1);
+		}
+		return false;
+	}
 
-  public boolean containsWord(String keyword)
-  {
-  }
+	/* method 2 */
+	public boolean containsWord(String keyword) {
+		return (" " + description + " ").indexOf(" disk ") != -1;
+	}
 
+	public String getMachineId() {
+		return machineId;
+	}
 
+	public String getDescription() {
+		return description;
+	}
 
-  public String getMachineId()
-  { return machineId; }
-
-  public String getDescription()
-  { return description; }
-
-  public String toString()
-  {
-    return getMachineId() + ":" + getDescription();
-  }
+	public String toString() {
+		return getMachineId() + ":" + getDescription();
+	}
 }
--- APCS/FRQ/x2016S(u)/A2/SystemLog.java
+++ APCS/FRQ/x2016S(u)/A2/SystemLog.java
@@ -1,31 +1,33 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class SystemLog
-{
-  private List<LogMessage> messageList;
+public class SystemLog {
+	private List<LogMessage> messageList;
 
-  public SystemLog(String[] messages)
-  {
-    messageList = new ArrayList<LogMessage>();
-    for (String s : messages)
-      messageList.add(new LogMessage(s));
-  }
+	public SystemLog(String[] messages) {
+		messageList = new ArrayList<LogMessage>();
+		for (String s : messages)
+			messageList.add(new LogMessage(s));
+	}
 
-  /* Part (c) */
+	/* Part (c) */
 
-  public List<LogMessage> removeMessages(String keyword)
-  {
-   
-      
-  }
+	public List<LogMessage> removeMessages(String keyword) {
+		List<LogMessage> al = new ArrayList<LogMessage>();
+		for (int i = 0; i < messageList.size(); i++) {
+			if (messageList.get(i).containsWord(keyword)) {
+				al.add(messageList.remove(i));
+				i--;
+			}
+		}
+		return al;
+	}
 
-  public String toString()
-  {
-    String s = "";
-    for (LogMessage msg : messageList)
-      s += msg + "\n";
-    return s;
-  }
+	public String toString() {
+		String s = "";
+		for (LogMessage msg : messageList)
+			s += msg + "\n";
+		return s;
+	}
 
 }
