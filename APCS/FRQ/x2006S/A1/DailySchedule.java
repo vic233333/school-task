29ae86b95e8ee1ce11810f6fde9fff138166a5fd
vic233333 <ddbb520db@163.com>
2023-03-31 16:37:14 +0800

    2006 A1 done

--- APCS/FRQ/x2006S/A1/DailySchedule.java
+++ APCS/FRQ/x2006S/A1/DailySchedule.java
@@ -1,39 +1,78 @@
 import java.util.ArrayList;
 
-public class DailySchedule
-{
-  // contains Appointment objects, no two Appointments overlap
-  private ArrayList apptList;
+public class DailySchedule {
+	// contains Appointment objects, no two Appointments overlap
+	private ArrayList<Appointment> apptList;
+
+	public DailySchedule() {
+		apptList = new ArrayList();
+	}
 
 
-  public DailySchedule()
-  {  apptList = new ArrayList();  }
+	/*** Part (b) ***/
 
+	// removes all appointments that overlap the given Appointment
+	// postcondition: all appointments that have a time conflict with
+	//                appt have been removed from this DailySchedule
+	public void clearConflicts(Appointment appt) {
+		for (int i = 0; i < apptList.size(); i++) {
+			if (apptList.get(i).conflictsWith(appt)) {
+				apptList.remove(i);
+				i--;
+			}
+		}
+	}
 
-  /*** Part (b) ***/
+	/*** Part (c) ***/
 
-  // removes all appointments that overlap the given Appointment
-  // postcondition: all appointments that have a time conflict with
-  //                appt have been removed from this DailySchedule
-  public void clearConflicts(Appointment appt)
-  {
-    
-  }
+	// if emergency is true, clears any overlapping appointments and adds
+	// appt to this DailySchedule; otherwise, if there are no conflicting
+	// appointments, adds appt to this DailySchedule;
+	// returns true if the appointment was added;
+	// otherwise, returns false
+	/* method 1*/
+	public boolean addAppt(Appointment appt, boolean emergency) {
+		if (emergency == true) {
+			clearConflicts(appt);
+			apptList.add(appt);
+			return true;
+		} else {
+			boolean flag = false;
+			for (int i = 0; i < apptList.size(); i++) {
+				if (apptList.get(i).conflictsWith(appt)) {
+					flag = true;
+				}
+			}
+			if (flag == false) {
+				apptList.add(appt);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
 
-  /*** Part (c) ***/
+	/* method 2*/
+	public boolean addAppt(Appointment appt, boolean emergency) {
+		if (emergency == true) {
+			clearConflicts(appt);
+			apptList.add(appt);
+			return true;
+		} else {
+			int i = 0;
+			while (i < apptList.size() && !apptList.get(i).conflictsWith(appt)) {
+				i++;
+			}//check if there is a conflicts
+			if (i == apptList.size()) {//there is no conflicts
+				apptList.add(appt);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
 
-  // if emergency is true, clears any overlapping appointments and adds
-  // appt to this DailySchedule; otherwise, if there are no conflicting
-  // appointments, adds appt to this DailySchedule;
-  // returns true if the appointment was added;
-  // otherwise, returns false
-  public boolean addAppt(Appointment appt, boolean emergency)
-  {
-    
-
-    
-  }
-
-  public String toString()
-  { return apptList.toString(); }
+	public String toString() {
+		return apptList.toString();
+	}
 }
