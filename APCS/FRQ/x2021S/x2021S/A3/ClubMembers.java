665434bf94563578edd092df2d2aa51053d8a9e6
vic233333 <ddbb520db@163.com>
2023-03-31 16:35:48 +0800

    2021 A3 done

--- APCS/FRQ/x2021S/x2021S/A3/ClubMembers.java
+++ APCS/FRQ/x2021S/x2021S/A3/ClubMembers.java
@@ -2,59 +2,80 @@
 
 import java.util.ArrayList;
 
-public class ClubMembers
-{
-  private ArrayList<MemberInfo> memberList;
+public class ClubMembers {
+	private ArrayList<MemberInfo> memberList;
 
-  public ClubMembers()
-  {
-    memberList = new ArrayList<MemberInfo>();
-  }
+	public ClubMembers() {
+		memberList = new ArrayList<MemberInfo>();
+	}
 
-  public ClubMembers(MemberInfo[] members)
-  {
-    memberList = new ArrayList<MemberInfo>();
-    for (MemberInfo member : members)
-      memberList.add(member);
-  }
-  
-  /* Part (a) */
+	public ClubMembers(MemberInfo[] members) {
+		memberList = new ArrayList<MemberInfo>();
+		for (MemberInfo member : members)
+			memberList.add(member);
+	}
 
-  /** Adds new club members to memberList, as described in part (a).
-   *  Precondition: names is a non-empty array.
-   */
-  public void addMembers(String[] names, int gradYear)
-  {
-   
-  }
+	/* Part (a) */
 
-  /* Part (b) */
-  
-  /** Removes members who have graduated and returns a list of members who have graduated
-   *  and are in good standing, as described in part (b).
-   */
-  public ArrayList<MemberInfo> removeMembers(int year)
-  {
-   
-  }
+	/**
+	 * Adds new club members to memberList, as described in part (a).
+	 * Precondition: names is a non-empty array.
+	 */
+	/* 题目要求：乱序添加 */
+	public void addMembers(String[] names, int gradYear) {
+		for (int i = 0; i < names.length; i++) {
+			MemberInfo temp = new MemberInfo(names[i], gradYear, true);
+			memberList.add(temp);
+		}
+	}
 
-  /***************** Test *****************/
+	/* 提高要求：按年份顺序添加 */
+	public void addMembers(String[] names, int gradYear) {
+		for (int i = 0; i < names.length; i++) {
+			MemberInfo temp = new MemberInfo(names[i], gradYear, true);
+			int j = 0;
+			while (j < names.length && gradYear > memberList.get(j).getGradYear()) {
+				j++;
+			}
+			memberList.add(j, temp);
+		}
+	}
 
-  public String toString()
-  {
-    return memberList.toString();
-  } 
+	/* Part (b) */
 
-  public static void main(String[] args)
-  {
-    MemberInfo[] members = {new MemberInfo("SMITH, JANE", 2019, false),
-                            new MemberInfo("FOX, STEVE", 2018, true),
-                            new MemberInfo("XIN, MICHAEL", 2017, false),
-                            new MemberInfo("GARCIA, MARIA", 2020, true)};
+	/**
+	 * Removes members who have graduated and returns a list of members who have graduated
+	 * and are in good standing, as described in part (b).
+	 */
+	public ArrayList<MemberInfo> removeMembers(int year) {
+		ArrayList<MemberInfo> al = new ArrayList<MemberInfo>();
+		for (int i = 0; i < memberList.size(); i++) {
+			if (memberList.get(i).getGradYear() <= year) {
+				MemberInfo temp = memberList.remove(i);
+				i--;
+				if (temp.inGoodStanding()) {
+					al.add(temp);
+				}
+			}
+		}
+		return al;
+	}
 
-    ClubMembers club = new ClubMembers(members);
+	/***************** Test *****************/
 
-    ArrayList<MemberInfo> newList = club.removeMembers(2018);
-    System.out.println("New list = " + newList + " Club list = " + club);
-  }
+	public String toString() {
+		return memberList.toString();
+	}
+
+	public static void main(String[] args) {
+		MemberInfo[] members = {new MemberInfo("SMITH, JANE", 2019, false),
+				new MemberInfo("FOX, STEVE", 2018, true),
+				new MemberInfo("XIN, MICHAEL", 2017, false),
+				new MemberInfo("GARCIA, MARIA", 2020, true)};
+
+		ClubMembers club = new ClubMembers(members);
+
+		ArrayList<MemberInfo> newList = club.removeMembers(2018);
+		System.out.println("New list = " + newList + " Club list = " + club);
+	}
 }
