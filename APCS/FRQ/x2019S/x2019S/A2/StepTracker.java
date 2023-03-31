be0b658225525bd2b2b1b15cdf177f9b6609c1aa
vic233333 <ddbb520db@163.com>
2023-03-31 16:38:43 +0800

    2019 A2 done

--- APCS/FRQ/x2019S/x2019S/A2/StepTracker.java
+++ APCS/FRQ/x2019S/x2019S/A2/StepTracker.java
@@ -1,5 +1,35 @@
 /* A2 */
 
-public class StepTracker
-{
+public class StepTracker {
+	private int actSteps;
+	private int activeDays;
+	private double sum;
+	private int count;
+
+	public StepTracker(int actSteps1) {
+		actSteps = actSteps1;
+		activeDays = 0;
+		sum = 0.0;
+		count = 0;
+	}
+
+	public int activeDays() {
+		return activeDays;
+	}
+
+	public double averageSteps() {
+		if (count == 0) {
+			return 0.0;
+		} else {
+			return sum / count;
+		}
+	}
+
+	public void addDailySteps(int steps2) {
+		sum += steps2;
+		count++;
+		if (steps2 >= actSteps) {
+			activeDays++;
+		}
+	}
 }
