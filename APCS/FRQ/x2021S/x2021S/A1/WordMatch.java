86b38d7e3c96dd2cae5ea40f3472b3f4f2182a33
vic233333 <ddbb520db@163.com>
2023-03-31 16:38:52 +0800

    2021 A1 done

--- APCS/FRQ/x2021S/x2021S/A1/WordMatch.java
+++ APCS/FRQ/x2021S/x2021S/A1/WordMatch.java
@@ -1,64 +1,84 @@
 /* A1 */
 
-public class WordMatch
-{
-  /** The secret string. */
-  private String secret;
+public class WordMatch {
+	/**
+	 * The secret string.
+	 */
+	private String secret;
 
-  /** Constructs a WordMatch object with the given secret string of lowercase letters. */
-  public WordMatch(String word)
-  {
-    secret = word;
-  }
+	/**
+	 * Constructs a WordMatch object with the given secret string of lowercase letters.
+	 */
+	public WordMatch(String word) {
+		secret = word;
+	}
 
-  /* Part (a) */
-  
-  /** Returns a score for guess, as described in part (a).
-   *  Precondition: 0 < guess.length() <= secret.length()
-   */
-  public int scoreGuess(String guess)
-  {
-    
-  }
+	/* Part (a) */
 
-  /* Part (b) */
+	/**
+	 * Returns a score for guess, as described in part (a).
+	 * Precondition: 0 < guess.length() <= secret.length()
+	 */
+	public int scoreGuess(String guess) {
+		int count = 0;
+		int i = secret.indexOf(guess);
+		while (i != -1) {
+			count++;
+			i = secret.indexOf(guess, i + 1);//1+1 ove
+		}
+		return count * guess.length() * guess.length();
+	}
 
-  /** Returns the better of two guesses, as determined by scoreGuess and the rules for a
-   *  tie-breaker that are described in part (b).
-   *  Precondition: guess1 and guess2 contain all lowercase letters.
-   *  guess1 is not the same as guess2.
-   */
-  public String findBetterGuess(String guess1, String guess2)
-  { 
-   }
+	/* Part (b) */
 
-  /***************** Test *****************/
-  
-  public static void main(String[] args)
-  {
-    WordMatch game = new WordMatch("mississippi");
-    String[] guesses1 = {"i", "iss", "issipp", "mississippi"};
-    for (String guess : guesses1)
-      System.out.println("Guess = " + guess + ", scoreGuess = " + game.scoreGuess(guess));
-    System.out.println();
+	/**
+	 * Returns the better of two guesses, as determined by scoreGuess and the rules for a
+	 * tie-breaker that are described in part (b).
+	 * Precondition: guess1 and guess2 contain all lowercase letters.
+	 * guess1 is not the same as guess2.
+	 */
+	public String findBetterGuess(String guess1, String guess2) {
+		int a1 = scoreGuess(guess1);
+		int a2 = scoreGuess(guess2);
+		if (a1 > a2) {
+			return guess1;
+		} else if (a1 < a2) {
+			return guess2;
+		} else {
+			if (guess1.compareTo(guess2) > 0) {
+				return guess1;
+			} else {
+				return guess2;
+			}
+		}
+	}
 
-    game = new WordMatch("aaaabb");
-    String[] guesses2 = {"a", "aa", "aaa", "aabb", "c"};
-    for (String guess : guesses2)
-      System.out.println("Guess = " + guess + ", scoreGuess = " + game.scoreGuess(guess));
-    System.out.println();
+	/***************** Test *****************/
 
-    game = new WordMatch("concatenation");
-    String guess1 = "ten";
-    String guess2 = "nation";
-    System.out.println("Guess = " + guess1 + ", scoreGuess = " + game.scoreGuess(guess1) + " " + 
-                       "Guess = " + guess2 + ", scoreGuess = " + game.scoreGuess(guess2) + " ==> " +
-                       game.findBetterGuess(guess1 , guess2));
-    
-    guess1 = "con";
-    guess2 = "cat";
-    System.out.println("Guess = " + guess1 + ", scoreGuess = " + game.scoreGuess(guess1) + " " + 
-                       "Guess = " + guess2 + ", scoreGuess = " + game.scoreGuess(guess2) + " ==> " +
-                       game.findBetterGuess(guess1, guess2));
-  }
+	public static void main(String[] args) {
+		WordMatch game = new WordMatch("mississippi");
+		String[] guesses1 = {"i", "iss", "issipp", "mississippi"};
+		for (String guess : guesses1)
+			System.out.println("Guess = " + guess + ", scoreGuess = " + game.scoreGuess(guess));
+		System.out.println();
+
+		game = new WordMatch("aaaabb");
+		String[] guesses2 = {"a", "aa", "aaa", "aabb", "c"};
+		for (String guess : guesses2)
+			System.out.println("Guess = " + guess + ", scoreGuess = " + game.scoreGuess(guess));
+		System.out.println();
+
+		game = new WordMatch("concatenation");
+		String guess1 = "ten";
+		String guess2 = "nation";
+		System.out.println("Guess = " + guess1 + ", scoreGuess = " + game.scoreGuess(guess1) + " " +
+				"Guess = " + guess2 + ", scoreGuess = " + game.scoreGuess(guess2) + " ==> " +
+				game.findBetterGuess(guess1, guess2));
+
+		guess1 = "con";
+		guess2 = "cat";
+		System.out.println("Guess = " + guess1 + ", scoreGuess = " + game.scoreGuess(guess1) + " " +
+				"Guess = " + guess2 + ", scoreGuess = " + game.scoreGuess(guess2) + " ==> " +
+				game.findBetterGuess(guess1, guess2));
+	}
 }
