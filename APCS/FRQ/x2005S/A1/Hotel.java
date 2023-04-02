1c3f1a4c9d79ab67b157a48d8050d1f4dfc3e950
vic233333 <ddbb520db@163.com>
2023-04-02 10:47:13 +0800

    2005 A1 done

--- APCS/FRQ/x2005S/A1/Hotel.java
+++ APCS/FRQ/x2005S/A1/Hotel.java
@@ -1,60 +1,67 @@
 import java.util.ArrayList;
 
-public class Hotel
-{
-  private Reservation[] rooms;
-    // each element correcponds to a room in the hotel;
-    // if rooms[index[ is null, the room is empty;
-    // otherwise, it contains a reference to the Reservation
-    // for that room, such that
-    //  rooms[index].getRoomNumber() returns index
+public class Hotel {
+	private Reservation[] rooms;
+	// each element correcponds to a room in the hotel;
+	// if rooms[index[ is null, the room is empty;
+	// otherwise, it contains a reference to the Reservation
+	// for that room, such that
+	//  rooms[index].getRoomNumber() returns index
 
-  private ArrayList waitList;
-    // contains names of guests who have not yet been
-    // assigned a room because all rooms are full
+	private ArrayList<String> waitList;
+	// contains names of guests who have not yet been
+	// assigned a room because all rooms are full
 
-  public Hotel(int nRooms)
-  {
-    rooms = new Reservation[nRooms];
-    waitList = new ArrayList();
-  }
+	public Hotel(int nRooms) {
+		rooms = new Reservation[nRooms];
+		waitList = new ArrayList();
+	}
 
-  /*** Part (a) ***/
+	/*** Part (a) ***/
 
-  // if there are any empty rooms (rooms with no reservation),
-  // then create a reservation for an empty room for the
-  // specified guest and return the new Reservation;
-  // otherwise, add the guest to the end of waitList
-  // and return null
-  public Reservation requestRoom(String guestName)
-  {
-   
-  }
+	// if there are any empty rooms (rooms with no reservation),
+	// then create a reservation for an empty room for the
+	// specified guest and return the new Reservation;
+	// otherwise, add the guest to the end of waitList
+	// and return null
+	public Reservation requestRoom(String guestName) {
+		for (int i = 0; i < rooms.length; i++) {
+			if (rooms[i] == null) {
+				rooms[i] = new Reservation(guestName, i);
+				return rooms[i];
+			}
+		}
+		waitList.add(guestName);
+		return null;
+	}
 
-  /*** Part (b) ***/
+	/*** Part (b) ***/
 
-  // release the room associated with parameter res, effectively
-  // cancelling the reservation;
-  // if any names are stored in waitList, remove the first name
-  // and create a Reservation for this person in the room 
-  // reserved by res; return that new Reservation;
-  // if waitList is empty, mark the room specified by res as empty and
-  // return null
-  // precondition:  res is a valid Reservation for some room
-  //                in this hotel
-  public Reservation cancelAndReassign(Reservation res)
-  {
-    
-  }
+	// release the room associated with parameter res, effectively
+	// cancelling the reservation;
+	// if any names are stored in waitList, remove the first name
+	// and create a Reservation for this person in the room
+	// reserved by res; return that new Reservation;
+	// if waitList is empty, mark the room specified by res as empty and
+	// return null
+	// precondition:  res is a valid Reservation for some room
+	//                in this hotel
+	public Reservation cancelAndReassign(Reservation res) {
+		rooms[res.getRoomNumber()] = null;
+		if (waitList.size() > 0) {
+			return requestRoom(waitList.remove(0));
+		} else {
+			return null;
+		}
+	}
 
-  public String toString()
-  {
-    String s = "";
+	public String toString() {
+		String s = "";
 
-    for (int i = 0; i < rooms.length; i++)
-      s += rooms[i] + " ";
+		for (int i = 0; i < rooms.length; i++)
+			s += rooms[i] + " ";
 
-    s += "Waitlist: " + waitList;
-    return s;
-  }
+		s += "Waitlist: " + waitList;
+		return s;
+	}
 }
