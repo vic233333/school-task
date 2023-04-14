678bb432f8e9e95cfad61c082e02608171a7b8cd
vic233333 <ddbb520db@163.com>
2023-04-14 11:22:27 +0800

    2015 A1 fix

--- APCS/FRQ/x2015S/A1/DiverseArray.java
+++ APCS/FRQ/x2015S/A1/DiverseArray.java
@@ -37,9 +37,9 @@ public class DiverseArray {
 	 * false otherwise.
 	 */
 	public static boolean isDiverse(int[][] arr2D) {
-		int ans[] = rowSums(arr2D);
+		int[] ans = rowSums(arr2D);
 		for (int i = 0; i < ans.length; i++) {
-			for (int j = 0; j < ans.length; j++) {
+			for (int j = i + 1; j < ans.length; j++) {
 				if (ans[i] == ans[j]) {
 					return false;
 				}
@@ -51,15 +51,15 @@ public class DiverseArray {
 	/******************************************************************************/
 
 	public static void main(String[] args) {
-		int[][] mat1 = { { 1, 3, 2, 7, 3 },
-				{ 10, 10, 4, 6, 2 },
-				{ 5, 3, 5, 9, 6 },
-				{ 7, 6, 4, 2, 1 } };
+		int[][] mat1 = {{1, 3, 2, 7, 3},
+				{10, 10, 4, 6, 2},
+				{5, 3, 5, 9, 6},
+				{7, 6, 4, 2, 1}};
 
-		int[][] mat2 = { { 1, 1, 5, 3, 4 },
-				{ 12, 7, 6, 1, 9 },
-				{ 8, 11, 10, 2, 5 },
-				{ 3, 2, 3, 0, 6 } };
+		int[][] mat2 = {{1, 1, 5, 3, 4},
+				{12, 7, 6, 1, 9},
+				{8, 11, 10, 2, 5},
+				{3, 2, 3, 0, 6}};
 
 		int[] sums = rowSums(mat1);
 		System.out.println(Arrays.toString(sums));
