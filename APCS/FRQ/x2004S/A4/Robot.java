c41df75c4cd367fa1e79f37e4c6e49a520f612e4
vic233333 <ddbb520db@163.com>
2023-04-17 14:50:15 +0800

    2004 A4 done

--- APCS/FRQ/x2004S/A4/Robot.java
+++ APCS/FRQ/x2004S/A4/Robot.java
@@ -1,52 +1,84 @@
 import java.util.Arrays;
 
-public class Robot
-{
-  private int[] hall;
-  private int pos;
-  private boolean facingRight;
+public class Robot {
+	private int[] hall;
+	private int pos;
+	private boolean facingRight;
 
-  public Robot(int[] toys, int pos, boolean facingRight)
-  {
-    hall = new int[toys.length];
-    for (int i = 0; i < toys.length; i++)
-      hall[i] = toys[i];
-    this.pos = pos;
-    this.facingRight = facingRight;
-  }
+	public Robot(int[] toys, int pos, boolean facingRight) {
+		hall = new int[toys.length];
+		for (int i = 0; i < toys.length; i++)
+			hall[i] = toys[i];
+		this.pos = pos;
+		this.facingRight = facingRight;
+	}
 
-  // A-4, (a)
+	// A-4, (a)
 
-  public boolean forwardMoveBlocked()
-  {
-   
-  }
+	/**
+	 * returns true if this Robot has a wall immediately in
+	 * front of it, so that it cannot move forward:
+	 * otherwise, returns false
+	 */
+	public boolean forwardMoveBlocked() {
+		if (facingRight && pos == hall.length - 1) {
+			return true;
+		} else if (!facingRight && pos == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  // A-4, (b)
+	// A-4, (b)
 
-  private void move()
-  {
-    
-  }
+	/**
+	 * one move has been made according to the
+	 * specifications above and the state of this
+	 * Robot has been updated
+	 */
+	private void move() {
+		// 搬
+		if (hall[pos] > 0) {
+			hall[pos]--;
+		}
+		// 走
+		if (forwardMoveBlocked() == true) {
+			facingRight = !facingRight;
+		} else {
+			if (facingRight) {
+				pos++;
+			} else {
+				pos--;
+			}
+		}
+	}
 
-  // A-4, (c)
+	// A-4, (c)
 
-  public int clearHall()
-  {
-   
-  }
+	/**
+	 * no more items remain in the hallway;
+	 * returns the number of moves made
+	 */
+	public int clearHall() {
+		int numMoves = 0;
+		while (hallIsClear() != true) {
+			move();
+			numMoves++;
+		}
+		return numMoves;
+	}
 
-  private boolean hallIsClear()
-  {
-    int count = 0;
+	private boolean hallIsClear() {
+		int count = 0;
 
-    for (int i = 0; i < hall.length; i++)
-      count += hall[i];
+		for (int i = 0; i < hall.length; i++)
+			count += hall[i];
 
-    return count == 0;
-  }
+		return count == 0;
+	}
 
-  
+
 }
 
 
