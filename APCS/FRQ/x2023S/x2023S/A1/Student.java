7485588faabe92e47ea89163a41495a13ca2cce4
vic233333 <ddbb520db@163.com>
2023-04-23 08:00:35 +0800

    高二下期中 模考

---
+++ APCS/FRQ/x2023S/x2023S/A1/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    /* Returns the name of this Student. */
+    public String getName() {
+        /* implementation not shown */}
+
+    /** Returns the number of times this Student has missed class. */
+    public int getAbsenceCount() {
+        /* implementation not shown */ }
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
