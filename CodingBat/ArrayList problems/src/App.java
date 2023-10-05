6d9c9f153ca6342b5d9db9cb6496d2d8f903d2c3
Yao Xiao <samxiaowastaken@outlook.com>
2023-10-05 23:26:45 +0800

    Add listCatty

--- CodingBat/ArrayList problems/src/App.java
+++ CodingBat/ArrayList problems/src/App.java
@@ -53,4 +53,26 @@ public class App {
         numList = Arrays.asList(7, 4, 2, 3, 6, 7, 7, 8, 5);
         System.out.println(listYucky7);
     }
+
+    public List<String> listCatty(List<String> animalsList) {
+        List<String> ret=new ArrayList<String>();
+        //go through the list
+        for(int i=0;i<animalsList.size();i++){
+            //go through the string
+            //因为我们遍历的是第一个字母，也就是C，所以只要减去两位，也就是at就可以了
+            for(int j=0;j<animalsList.get(i).length()-2;j++){
+                //如果列表中的第i位字符串的第j~j+3位等于"cat"
+                if(animalsList.get(i).substring(j,j+3+1).equals("cat")){
+                    ret.add(animalsList.get(i));
+                }
+                //same as above
+                else if (animalsList.get(i).substring(j,j+3+1).equals("Cat")) {
+                    ret.add(animalsList.get(i));
+                }
+            }
+        }
+        return ret;
+    }
+
+
 }
